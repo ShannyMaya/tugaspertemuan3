0c518fda5682db27d11c363c1f4b0fdc6323262d
Shanny Mayasari <144967485+ShannyMaya@users.noreply.github.com>
2024-02-19 20:57:15 +0700

    Create Soal_Ganjil_3.java

---
+++ Soal_Ganjil_3.java
@@ -0,0 +1,14 @@
+public class Java3 {
+    public static void main(String[] args) {
+        char karakter1 = '1';
+        char karakter2 = '2';
+        
+        int ascii1 = (int) karakter1;
+        int ascii2 = (int) karakter2;
+        int sum = ascii1 + ascii2;
+        
+        System.out.println("Nilai ASCII dari '1' adalah " + ascii1);
+        System.out.println("Nilai ASCII dari '2' adalah " + ascii2);
+        System.out.println("Jumlah nilai ASCII adalah " + sum);
+    }
+}
